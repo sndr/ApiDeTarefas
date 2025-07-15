f3fd104cde2643dfb050268a986cd4a351870a9c
sndr <alexsanderbeny3707@gmail.com>
2025-07-15 10:42:32 +0200

    Deploying the base structure

---
+++ src/test/java/com/api/ApiDeTarefas/ApiDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ApiDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
