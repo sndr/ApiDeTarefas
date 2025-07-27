65681d1efc986c64dfe0ee97daa4bf03b0c1c484
sndr <alexsanderbeny3707@gmail.com>
2025-07-27 14:59:34 +0200

    ALL SET

--- src/main/java/com/api/ApiDeTarefas/model/Tarefa.java
+++ src/main/java/com/api/ApiDeTarefas/model/Tarefa.java
@@ -1,31 +1,32 @@
 package com.api.ApiDeTarefas.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Tarefa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nome")
+    @JsonProperty("nome")
+    @NotNull(message = "O nome da tarefa é obrigatório")
+    @Size(min = 1, max = 100, message = "O nome deve ter entre 1 e 100 caracteres")
     private String nome;
+
+    @Column(name = "data_entrega")
+    @JsonProperty("dataEntrega")
     private LocalDate dataEntrega;
+
+    @Column(name = "responsavel")
+    @JsonProperty("responsavel")
+    @NotNull(message = "O responsável é obrigatório")
+    @Size(min = 1, max = 100, message = "O responsável deve ter entre 1 e 100 caracteres")
     private String responsavel;
-
-    public Tarefa() {}
-
-    public Tarefa(String nome, LocalDate dataEntrega, String responsavel) {
-        this.nome = nome;
-        this.dataEntrega = dataEntrega;
-        this.responsavel = responsavel;
-    }
-
 }
---
+++ src/main/java/com/api/ApiDeTarefas/service/TarefaService.java
@@ -0,0 +1,35 @@
+package com.api.ApiDeTarefas.service;
+
+import com.api.ApiDeTarefas.model.Tarefa;
+import com.api.ApiDeTarefas.repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TarefaService {
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public Tarefa salvarTarefa(Tarefa tarefa) {
+        return tarefaRepository.save(tarefa);
+    }
+
+    public List<Tarefa> findAll() {
+        return tarefaRepository.findAll();
+    }
+
+    public Optional<Tarefa> findById(Long id) {
+        return tarefaRepository.findById(id);
+    }
+
+    public boolean existsById(Long id) {
+        return tarefaRepository.existsById(id);
+    }
+
+    public void deleteById(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+}
